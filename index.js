8d19227ae6e9f62a8e586dd57d6515e9109c5be9
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-06-20 15:11:04 +0300

    Connect to MongoDB database

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
 const app = express();
 const port = 3000;
+dotenv.config();
 
-
+// Middleware
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+// MongoDB connection
+const mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+// routing
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
