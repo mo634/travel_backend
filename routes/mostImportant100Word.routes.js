3235c66db9cb76ef0cc39687a398189b1c61d9f7
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-06-28 01:43:30 +0300

    update mostImportant100Word api

--- routes/mostImportant100Word.routes.js
+++ routes/mostImportant100Word.routes.js
@@ -1,7 +1,7 @@
-import express from 'express';
+import { Router } from 'express';
 import { upload, uploadPdf, updatePdf, deletePdf, getPdf } from '../controllers/mostImportant100Word.controller.js'; // Adjust the path as necessary
 
-const router = express.Router();
+const router = Router();
 
 router.post('/upload', upload.single('pdfFile'), uploadPdf);
 router.put('/update/:id', upload.single('pdfFile'), updatePdf);
